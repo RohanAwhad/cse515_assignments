3742b02845ab6601b26b3a09a4df178bccc06451
Rohan Awhad <rohanavad007@gmail.com>
2023-09-10 23:16:49 -0700

    not retrieving the same image anymore; retrieving the next K images

--- task3.py
+++ task3.py
@@ -49,7 +49,7 @@ def retrieve(img_id):
 
   color_mmt_similarity = torch.cat((color_mmt.unsqueeze(0), 
     torch.tensor(COLOR_MMT_FEATS)), dim=0).corrcoef()[0, 1:]  # pearson correlation coefficient
-  color_mmt_top_k_img_ids = color_mmt_similarity.numpy().argsort()[-K:][::-1]
+  color_mmt_top_k_img_ids = color_mmt_similarity.numpy().argsort()[-(K+1):-1][::-1]
   color_mmt_top_k_imgs = [ds[COLOR_MMT_IDX[x]][0] for x in color_mmt_top_k_img_ids]
   top_k_imgs.append(color_mmt_top_k_imgs)
   top_k_ids.append([COLOR_MMT_IDX[x] for x in color_mmt_top_k_img_ids])
@@ -58,25 +58,25 @@ def retrieve(img_id):
   hog = hog.unsqueeze(0).expand(len(HOG_FEATS), -1)
   hog_stack = torch.stack([hog, HOG_FEATS])
   hog_similarity = hog_stack.min(0).values.sum(-1) / hog_stack.max(0).values.sum(-1)
-  hog_top_k_img_ids = hog_similarity.numpy().argsort()[-K:][::-1]
+  hog_top_k_img_ids = hog_similarity.numpy().argsort()[-(K+1):-1][::-1]
   hog_top_k_imgs = [ds[HOG_IDX[x]][0] for x in hog_top_k_img_ids]
   top_k_imgs.append(hog_top_k_imgs)
   top_k_ids.append([HOG_IDX[x] for x in color_mmt_top_k_img_ids])
 
   resnet_avgpool_similarity = (resnet_avgpool @ RESNET_AVGPOOL_FEATS.T) / (norm(resnet_avgpool) * norm(RESNET_AVGPOOL_FEATS.T, axis=0))  # cosine similarity
-  resnet_avgpool_top_k_img_ids = resnet_avgpool_similarity.argsort()[-K:][::-1]
+  resnet_avgpool_top_k_img_ids = resnet_avgpool_similarity.argsort()[-(K+1):-1][::-1]
   resnet_avgpool_top_k_imgs = [ds[RESNET_AVGPOOL_IDX[x]][0] for x in resnet_avgpool_top_k_img_ids]
   top_k_imgs.append(resnet_avgpool_top_k_imgs)
   top_k_ids.append([RESNET_AVGPOOL_IDX[x] for x in color_mmt_top_k_img_ids])
 
   resnet_layer3_similarity = (resnet_layer3 @ RESNET_LAYER3_FEATS.T) / (norm(resnet_layer3) * norm(RESNET_LAYER3_FEATS.T, axis=0))  # cosine similarity
-  resnet_layer3_top_k_img_ids = resnet_layer3_similarity.argsort()[-K:][::-1]
+  resnet_layer3_top_k_img_ids = resnet_layer3_similarity.argsort()[-(K+1):-1][::-1]
   resnet_layer3_top_k_imgs = [ds[RESNET_LAYER3_IDX[x]][0] for x in resnet_layer3_top_k_img_ids]
   top_k_imgs.append(resnet_layer3_top_k_imgs)
   top_k_ids.append([RESNET_LAYER3_IDX[x] for x in color_mmt_top_k_img_ids])
 
   resnet_fc_similarity = np.abs(resnet_fc[np.newaxis, :] - RESNET_FC_FEATS).sum(-1)  # manhattan distance
-  resnet_fc_top_k_img_ids = resnet_fc_similarity.argsort()[:K]
+  resnet_fc_top_k_img_ids = resnet_fc_similarity.argsort()[1:K+1]
   resnet_fc_top_k_imgs = [ds[RESNET_FC_IDX[x]][0] for x in resnet_fc_top_k_img_ids]
   top_k_imgs.append(resnet_fc_top_k_imgs)
   top_k_ids.append([RESNET_FC_IDX[x] for x in color_mmt_top_k_img_ids])
