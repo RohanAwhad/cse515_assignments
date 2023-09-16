1691d0fcdcbd9b45a8e6d3fd885b8f28225f25aa
Rohan Awhad <rohanavad007@gmail.com>
2023-09-16 12:23:15 -0700

    refactoring retriever to similariy metrics

---
+++ similarity_metrics.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+
+from numpy.linalg import norm
+
+def cosine_similarity(query_vec, db_mat, idx_dict, K):
+  query_vec = query_vec.numpy()
+  similarity_scores = (query_vec @ db_mat.T) / (norm(query_vec) * norm(db_mat.T, axis=0))  # cosine similarity
+  ss_arg_idx = similarity_scores.argsort()[-K:][::-1]
+  top_k_scores = [similarity_scores[x] for x in ss_arg_idx]
+  top_k_idx = [idx_dict[x][0] for x in ss_arg_idx]
+  return top_k_idx, top_k_scores
+
+
+def intersection_similarity(query_vec, db_mat, idx_dict, K):
+  query_vec = query_vec.unsqueeze(0).expand(len(db_mat), -1)
+  stack_ = torch.stack([query_vec, db_mat])
+  similarity_scores = stack_.min(0).values.sum(-1) / stack_.max(0).values.sum(-1)
+  ss_arg_idx = similarity_scores.numpy().argsort()[-K:][::-1]
+  top_k_scores = [similarity_scores[x] for x in ss_arg_idx]
+  top_k_idx = [idx_dict[x][0] for x in ss_arg_idx]
+  return top_k_idx, top_k_scores
+
+
+def pearson_coefficient(query_vec, db_mat, idx_dict, K):
+  similarity_scores = torch.cat((query_vec.unsqueeze(0), 
+    torch.tensor(db_mat)), dim=0).corrcoef()[0, 1:]  # pearson correlation coefficient
+  ss_arg_idx = similarity_scores.numpy().argsort()[-K:][::-1]  # TODO (rohan): is conversion to numpy compulsory?
+  top_k_scores = [similarity_scores[x] for x in ss_arg_idx]
+  top_k_idx = [idx_dict[x][0] for x in ss_arg_idx]
+  return top_k_idx, top_k_scores
+
+
+def manhattan_distance(query_vec, db_mat, idx_dict, K):
+  query_vec = query_vec.numpy()
+  similarity_scores = np.abs(query_vec[np.newaxis, :] - db_mat).sum(-1)  # manhattan distance
+  ss_arg_idx = similarity_scores.argsort()[:K]
+  top_k_scores = [similarity_scores[x] for x in ss_arg_idx]
+  top_k_idx = [self.resnet_layer3_idx[x][0] for x in ss_arg_idx]
+  return top_k_idx, top_k_scores
+
--- task3.py
+++ task3.py
@@ -2,6 +2,7 @@
 
 import config
 import helper
+import similarity_metrics
 
 from feature_descriptor import FeatureDescriptor
 
@@ -26,33 +27,105 @@ RESNET_LAYER3_IDX, RESNET_LAYER3_FEATS = helper.load_data('resnet_layer3')
 RESNET_FC_IDX, RESNET_FC_FEATS = helper.load_data('resnet_fc')
 
 HOG_FEATS = torch.tensor(HOG_FEATS)
+class Retriever:
+# TODO (rohan): Instead of having feature wise retrieval, wouldn't it better if we could have distance calculation functions, which would give back similarity scores and indices, based on argmin, or argmax
+  def __init__(self,
+    color_mmt_idx,
+    color_mmt_feats,
+    hog_idx,
+    hog_feats,
+    resnet_avgpool_idx,
+    resnet_avgpool_feats,
+    resnet_layer3_idx,
+    resnet_layer3_feats,
+    resnet_fc_idx,
+    resnet_fc_feats,
+  ):
+    self.color_mmt_feats = color_mmt_feats
+    self.hog_feats = hog_feats
+    self.resnet_avgpool_feats = resnet_avgpool_feats
+    self.resnet_layer3_feats = resnet_layer3_feats
+    self.resnet_fc_feats = resnet_fc_feats
 
-def retrieve(img_id):
+    self.color_mmt_idx = color_mmt_idx
+    self.hog_idx = hog_idx
+    self.resnet_avgpool_idx = resnet_avgpool_idx
+    self.resnet_layer3_idx = resnet_layer3_idx
+    self.resnet_fc_idx = resnet_fc_idx
+
+  def retrieve_using_color_moments(self, query_feats):
+    '''Retrieve using Pearson Coefficient'''
+    return similarity_metrics.pearson_coefficient(query_feats, self.color_mmt_feats, self.color_mmt_idx, K)
+
+  def retrieve_using_hog(self, query_feats):
+    '''Retrieve using Intersection Similarity'''
+    return similarity_metrics.intersection_similarity(query_feats, self.hog_feats, self.hog_idx, K)
+
+  def retrieve_using_resnet_avgpool(self, query_feats):
+    '''Retrieve using Cosine Similarity '''
+    return similarity_metrics.cosine_similarity(query_feats, self.resnet_avgpool_feats, self.resnet_avgpool_idx, K)
+
+  def retrieve_using_resnet_layer3(self, query_feats):
+    '''Retrieve using Cosine Similarity '''
+    return similarity_metrics.cosine_similarity(query_feats, self.resnet_layer3_feats, self.resnet_layer3_idx, K)
+
+  def retrieve_using_resnet_fc(self, query_feats):
+    '''Retrieve using Manhattan Distance'''
+    return similarity_metrics.manhattan_distance(query_feats, self.resnet_fc_feats, self.resnet_fc_idx, K)
+    query_feats = query_feats.numpy()
+    similarity_scores = np.abs(query_feats[np.newaxis, :] - self.resnet_fc_feats).sum(-1)  # manhattan distance
+    ss_arg_idx = similarity_scores.argsort()[:K]
+    top_k_scores = [similarity_scores[x] for x in ss_arg_idx]
+    top_k_idx = [self.resnet_layer3_idx[x][0] for x in ss_arg_idx]
+    return top_k_idx, top_k_scores
+
+
+retriever = Retriever(
+  COLOR_MMT_IDX,
+  COLOR_MMT_FEATS,
+  HOG_IDX,
+  HOG_FEATS,
+  RESNET_AVGPOOL_IDX,
+  RESNET_AVGPOOL_FEATS,
+  RESNET_LAYER3_IDX,
+  RESNET_LAYER3_FEATS,
+  RESNET_FC_IDX,
+  RESNET_FC_FEATS,
+)
+
+FEAT_DESC_TO_SIMILARITY_METRIC = {
+  'color_moment': 'pearson_coefficient',
+  'hog': 'intersection_similarity',
+  'resnet_avgpool': 'cosine_similarity',
+  'resnet_layer3': 'cosine_similarity',
+  'resnet_fc': 'manhattan_distance'
+}
+FEAT_DESC_TO_RETRIEVAL_FUNC = {
+  'color_moment': retriever.retrieve_using_color_moments,
+  'hog': retriever.retrieve_using_hog,
+  'resnet_avgpool': retriever.retrieve_using_resnet_avgpool,
+  'resnet_layer3': retriever.retrieve_using_resnet_layer3,
+  'resnet_fc': retriever.retrieve_using_resnet_fc,
+}
+
+def retrieve(img_id, feature_desc):
   img = config.DATASET[img_id][0]
   if img.mode != 'RGB': img = img.convert('RGB')
 
+  '''
   color_mmt = feature_descriptor.extract_color_moments(img)#.numpy()
   hog = feature_descriptor.extract_hog_features(img)
   resnet_layer3, resnet_avgpool, resnet_fc = tuple(map(lambda x: x.numpy(), feature_descriptor.extract_resnet_features(img)))
+  '''
 
-  top_k_imgs = []
-  top_k_ids = []
+  #top_k_ids, top_k_scores = retriever.retrieve_using_resnet_avgpool(resnet_avgpool)
+  top_k_ids, top_k_scores = FEAT_DESC_TO_RETRIEVAL_FUNC[feature_desc](feature_descriptor.extract_features(img, feature_desc))
+  top_k_imgs = [config.DATASET[x][0] for x in top_k_ids]
+  helper.plot(img, img_id, top_k_imgs, top_k_ids, top_k_scores, K, feature_desc, FEAT_DESC_TO_SIMILARITY_METRIC[feature_desc])
 
-  color_mmt_similarity = torch.cat((color_mmt.unsqueeze(0), 
-    torch.tensor(COLOR_MMT_FEATS)), dim=0).corrcoef()[0, 1:]  # pearson correlation coefficient
-  color_mmt_top_k_img_ids = color_mmt_similarity.numpy().argsort()[-(K+1):-1][::-1]
-  color_mmt_top_k_imgs = [config.DATASET[COLOR_MMT_IDX[x]][0] for x in color_mmt_top_k_img_ids]
-  top_k_imgs.append(color_mmt_top_k_imgs)
-  top_k_ids.append([COLOR_MMT_IDX[x] for x in color_mmt_top_k_img_ids])
+  '''
 
   # intersection similarity
-  hog = hog.unsqueeze(0).expand(len(HOG_FEATS), -1)
-  hog_stack = torch.stack([hog, HOG_FEATS])
-  hog_similarity = hog_stack.min(0).values.sum(-1) / hog_stack.max(0).values.sum(-1)
-  hog_top_k_img_ids = hog_similarity.numpy().argsort()[-(K+1):-1][::-1]
-  hog_top_k_imgs = [config.DATASET[HOG_IDX[x]][0] for x in hog_top_k_img_ids]
-  top_k_imgs.append(hog_top_k_imgs)
-  top_k_ids.append([HOG_IDX[x] for x in hog_top_k_img_ids])
 
   resnet_avgpool_similarity = (resnet_avgpool @ RESNET_AVGPOOL_FEATS.T) / (norm(resnet_avgpool) * norm(RESNET_AVGPOOL_FEATS.T, axis=0))  # cosine similarity
   resnet_avgpool_top_k_img_ids = resnet_avgpool_similarity.argsort()[-(K+1):-1][::-1]
@@ -73,9 +146,15 @@ def retrieve(img_id):
   top_k_ids.append([RESNET_FC_IDX[x] for x in resnet_fc_top_k_img_ids])
 
   helper.save_top_k(img, img_id, top_k_imgs, top_k_ids, K, f'outputs/{img_id}.png')
+  '''
 
 if __name__ == '__main__':
   while True:
     img_id = int(input(f'Enter an image id [0, {len(config.DATASET)-1}]: '))
     if img_id < 0 or img_id >= len(config.DATASET): print(f'img id invalid. should be between [0, {len(config.DATASET)-1}], try again. you got it!')
-    else: retrieve(img_id)
+    else:
+      retrieve(img_id, 'color_moment')
+      retrieve(img_id, 'hog')
+      retrieve(img_id, 'resnet_avgpool')
+      retrieve(img_id, 'resnet_layer3')
+      retrieve(img_id, 'resnet_fc')
