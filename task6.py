6083066eb4e6eddd3fde4d58b843f44bc48eef68
Rohan Awhad <rohanavad007@gmail.com>
2023-10-06 14:54:08 -0700

    added print statement for image-weight pairs

--- task6.py
+++ task6.py
@@ -3,38 +3,32 @@
 import config
 import helper
 import dimensionality_reduction
-
 from similarity_metrics import get_similarity, get_similarity_mat_x_mat
 
-#3rd-party libs
-import functools
-import torch
-import numpy as np
 
-if __name__ == '__main__':
+def print_img_id_weight_pairs(weight_mat, idx_dict):
+  for i, row in enumerate(weight_mat):
+    print()
+    print(f'Image ID: {idx_dict[i][0]:4d} | Label: {idx_dict[i][1]:4d}')
+    row = [(x, j) for j, x in enumerate(row)]
+    row = sorted(row, key=lambda x: x[0], reverse=True)
+    for x, j in row:
+      print(f' - Latent Feat {j:3d}\t\tWeight := {x:10.3f}')
+
+  print('='*53)
+
+
+def main():
   inp = helper.get_user_input('feat_space,K,dim_red')
-  '''
-  inp = {
-    'feat_space': 'color_moment',
-    #'feat_space': 'hog',
-    #'feat_space': 'resnet_layer3',
-    #'feat_space': 'resnet_avgpool',
-    #'feat_space': 'resnet_fc',
-    'K': 5,
-    'dim_red': 'svd'
-  }
-  '''
-  #feat_db = config.FEAT_DESC_FUNCS[inp['feat_space']][config.FEAT_DB]
-  #TODO (rohan): feat_db of label-label similarity matrix
   _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
   feat_db = _tmp[config.FEAT_DB]
   similarity_metric = _tmp[config.SIMILARITY_METRIC]
+  idx_dict = _tmp[config.IDX]
 
-  if not isinstance(feat_db, torch.Tensor): feat_db = torch.tensor(feat_db)
   similarity_scores = get_similarity_mat_x_mat(feat_db, feat_db, similarity_metric)
-  print(similarity_scores.shape)
   W, H = dimensionality_reduction.reduce_(similarity_scores, inp['K'], inp['dim_red'])
-  print(W.shape, H.shape)
   helper.save_pickle(H, config.LATENT_SEMANTICS_FN.format(task='6_img_img_simi_mat', **inp))
+  print_img_id_weight_pairs(W, idx_dict)
 
-  
+if __name__ == '__main__':
+  while True: main()
