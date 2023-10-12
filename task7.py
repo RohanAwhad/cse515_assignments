2d8106fa2aeda5530694843020f7d4d381d1484b
Shazeb Ata <shazebata@Shazebs-MacBook-Air.local>
2023-10-12 02:04:09 -0700

    refactored

--- task7.py
+++ task7.py
@@ -15,74 +15,48 @@ feature_descriptor = FeatureDescriptor(net=config.RESNET_MODEL)
 
 def main():
   inp = helper.get_user_input('task_id,K,feat_space,img_id,K_latent',len(config.DATASET))
+  _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
+  feat_db, idx_dict, similarity_metric = _tmp[config.FEAT_DB], _tmp[config.IDX], _tmp[config.SIMILARITY_METRIC]
+  if isinstance(inp['img_id'], str): img = helper.load_img_file(inp['img_id'])
+  else: img = config.DATASET[inp['img_id']][0]
+  if img.mode != 'RGB': img = img.convert('RGB')  
+
   if(inp['task_id']==3):
     inp_2 = helper.get_user_input('dim_red',len(config.DATASET))
     inp['dim_red'] = inp_2['dim_red']
-    _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
-    feat_db, idx_dict, similarity_metric = _tmp[config.FEAT_DB], _tmp[config.IDX], _tmp[config.SIMILARITY_METRIC]
     latent_space = helper.load_semantics("task" + str(inp['task_id'])+ "_" + inp['feat_space']+"_"+inp['dim_red']+"_"+str(inp['K_latent']))
-    if isinstance(inp['img_id'], str): img = helper.load_img_file(inp['img_id'])
-    else: img = config.DATASET[inp['img_id']][0]
-    if img.mode != 'RGB': img = img.convert('RGB')
     query_feat = feature_descriptor.extract_features(img, inp['feat_space'])
-    train_latent_space = np.dot(feat_db, np.transpose(latent_space))
-    train_latent_space = torch.tensor(train_latent_space)
-    query_latent_space = np.dot(query_feat, np.transpose(latent_space))
-    query_latent_space = torch.tensor(query_latent_space)
+
 
   elif(inp['task_id']==4):
     inp['dim_red']="cp"
-    _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
-    feat_db, idx_dict, similarity_metric = _tmp[config.FEAT_DB], _tmp[config.IDX], _tmp[config.SIMILARITY_METRIC]
     latent_space = helper.load_semantics("task" + str(inp['task_id'])+ "_" + inp['feat_space']+"_"+inp['dim_red']+"_"+str(inp['K_latent'])+"_feature.pkl")
-    if isinstance(inp['img_id'], str): img = helper.load_img_file(inp['img_id'])
-    else: img = config.DATASET[inp['img_id']][0]
-    if img.mode != 'RGB': img = img.convert('RGB')
     query_feat = feature_descriptor.extract_features(img, inp['feat_space'])
-    train_latent_space = np.dot(feat_db, np.transpose(latent_space))
-    train_latent_space = torch.tensor(train_latent_space)
-    query_latent_space = np.dot(query_feat, np.transpose(latent_space))
-    query_latent_space = torch.tensor(query_latent_space)
+    
 
   elif(inp['task_id']==5):
     inp_2 = helper.get_user_input('dim_red',len(config.DATASET))
     inp['dim_red'] = inp_2['dim_red']
     label_feat_sp = get_label_vecs(inp['feat_space']) #num_labelsX#num_features
-    if isinstance(inp['img_id'], str): img = helper.load_img_file(inp['img_id'])
-    else: img = config.DATASET[inp['img_id']][0]
-
-    if img.mode != 'RGB': img = img.convert('RGB')
-
-    _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
-    feat_db, idx_dict, similarity_metric = _tmp[config.FEAT_DB], _tmp[config.IDX], _tmp[config.SIMILARITY_METRIC]
     latent_space = helper.load_semantics("task" + str(inp['task_id']) + "_label_label_simi_mat"+ "_" + inp['feat_space']+"_"+inp['dim_red']+"_"+str(inp['K_latent'])+".pkl")
     query_feat = feature_descriptor.extract_features(img, inp['feat_space'])
     query_feat = get_similarity_mat_x_mat(query_feat.unsqueeze(0), label_feat_sp, similarity_metric)
     feat_db = get_similarity_mat_x_mat(feat_db, label_feat_sp, similarity_metric)
-    train_latent_space = np.dot(feat_db, np.transpose(latent_space))
-    train_latent_space = torch.tensor(train_latent_space)
-    query_latent_space = np.dot(query_feat, np.transpose(latent_space))
-    query_latent_space = torch.tensor(query_latent_space)
+    
 
   elif(inp['task_id']==6):
     inp_2 = helper.get_user_input('dim_red',len(config.DATASET))
     inp['dim_red'] = inp_2['dim_red']
-    if isinstance(inp['img_id'], str): img = helper.load_img_file(inp['img_id'])
-    else: img = config.DATASET[inp['img_id']][0]
-
-    if img.mode != 'RGB': img = img.convert('RGB')
-
-    _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
-    feat_db, idx_dict, similarity_metric = _tmp[config.FEAT_DB], _tmp[config.IDX], _tmp[config.SIMILARITY_METRIC]
     latent_space = helper.load_semantics("task" + str(inp['task_id']) + "_img_img_simi_mat"+ "_" + inp['feat_space']+"_"+inp['dim_red']+"_"+str(inp['K_latent'])+".pkl")
     query_feat = feature_descriptor.extract_features(img, inp['feat_space'])
     query_feat = get_similarity_mat_x_mat(query_feat.unsqueeze(0), feat_db, similarity_metric)
     feat_db = get_similarity_mat_x_mat(feat_db, feat_db, similarity_metric)
-    train_latent_space = np.dot(feat_db, np.transpose(latent_space))
-    train_latent_space = torch.tensor(train_latent_space)
-    query_latent_space = np.dot(query_feat, np.transpose(latent_space))
-    query_latent_space = torch.tensor(query_latent_space)
+    
 
+  train_latent_space = np.dot(feat_db, np.transpose(latent_space))
+  train_latent_space = torch.tensor(train_latent_space)
+  query_latent_space = np.dot(query_feat, np.transpose(latent_space))
+  query_latent_space = torch.tensor(query_latent_space)
   similarity_scores = get_similarity(query_latent_space, train_latent_space, similarity_metric)
   top_k_ids, top_k_scores = get_top_k_ids_n_scores(similarity_scores, idx_dict, inp['K'])
   top_k_imgs = [config.DATASET[x][0] for x in top_k_ids]
