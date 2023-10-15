fadcd6a23f2332924d3fd055fad80191c5df09bc
nikhilASU <114959558+nikhilASU@users.noreply.github.com>
2023-10-15 12:48:18 -0700

    Adding Kmeans to dimensionality_reduction

--- dimensionality_reduction.py
+++ dimensionality_reduction.py
@@ -4,7 +4,38 @@ from sklearn.decomposition import non_negative_factorization as NNMF
 from sklearn.cluster import k_means as KMeans
 from hottbox.core import Tensor, TensorTKD
 from hottbox.algorithms.decomposition import CPD
-import numpy
+from tqdm import tqdm
+import torch
+import numpy as np
+
+from similarity_metrics import get_similarity
+
+
+def recaliberate_centroids(cluster_idx, K, X):
+    _, n = np.shape(X)
+    centroids = np.empty((K, n))
+    for i in range(K):
+        points = X[cluster_idx == i] 
+        centroids[i] = np.mean(points, axis=0) 
+    return centroids
+
+def Kmeans(K, X, max_iterations=500):
+    m, n = np.shape(X)
+    centroids = np.empty((K, n))
+    for i in range(K):
+        centroids[i] =  X[np.random.choice(range(m))] 
+    for _ in tqdm(range(max_iterations)):
+        clusters = np.empty(m)
+        for i in range(m):
+            distances = np.empty(K)
+            for j in range(K):
+                distances[j] = np.sqrt(np.sum((centroids[j] - X[i])**2))
+            clusters[i] =  np.argmin(distances)
+        tmp_centroids = centroids                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        centroids = recaliberate_centroids(clusters, K, X)
+        if not (tmp_centroids - centroids).any():
+            return centroids, clusters 
+    return centroids, clusters
 
 def reduce_(feat_db, K, dim_red):
 
@@ -22,8 +53,9 @@ def reduce_(feat_db, K, dim_red):
     weight_mat, components, _ = NNMF(abs(feat_db), n_components=K)
 
   elif dim_red == 'kmeans':
-    components, *_ = KMeans(feat_db, n_clusters=K)  # TODO (rohan): calculate similarity between feat_db and W and return them as imageid-weight pairs
-    weight_mat = None
+    components, *_ = Kmeans(K, feat_db.numpy())
+    components = torch.tensor(components)
+    #weight_mat = get_similarity(components, feat_db, "euclidean_distance")
     raise NotImplementedError('for kmeans haven\'t yet calculated weight matrix')
   
   elif dim_red == 'cp':
