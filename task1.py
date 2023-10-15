53428900f24b48913116a36151d3422e7f255e28
Rohan Awhad <rohanavad007@gmail.com>
2023-10-15 12:42:38 -0700

    task1 runs!

---
+++ task1.py
@@ -0,0 +1,35 @@
+#!/opt/homebrew/bin/python3
+
+import config
+import helper
+
+from feature_descriptor import FeatureDescriptor
+from similarity_metrics import get_similarity, get_top_k_ids_n_scores
+
+# 3rd-party libs
+import torch
+
+feature_descriptor = FeatureDescriptor(net=config.RESNET_MODEL)
+
+def retrieve(label, feat_space, K):
+
+  # calc label vec
+  ## get images feats for the given label
+  *_, feat_db_idx, feat_db = config.FEAT_DESC_FUNCS[feat_space]
+  label_feats = [x[0] for x in filter(lambda x: x[1][1] == label, feat_db_idx.items())]
+  label_feat = torch.tensor(feat_db[label_feats, :].mean(0))
+
+  similarity_metric, feat_db_idx, feat_db = config.FEAT_DESC_FUNCS[feat_space]
+  similarity_scores = get_similarity(label_feat, feat_db, similarity_metric)
+  top_k_ids, top_k_scores = get_top_k_ids_n_scores(similarity_scores, feat_db_idx, K)
+  top_k_imgs = [config.DATASET[x][0] for x in top_k_ids]
+  helper.plot(None, None, top_k_imgs, top_k_ids, top_k_scores, K, feat_space, similarity_metric)
+
+if __name__ == '__main__':
+  while True:
+    inp = helper.get_user_input(
+      'K,feat_space,label',
+      len(config.DATASET),
+      len(set(config.DATASET.y))
+    )
+    retrieve(**inp)
