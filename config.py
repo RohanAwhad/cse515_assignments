4387a239739c68359b273e4d54d8df40e9f2c35c
Rohan Awhad <rohanavad007@gmail.com>
2023-10-15 11:04:46 -0700

    now saving the ims latent features along with components

--- config.py
+++ config.py
@@ -58,10 +58,14 @@ FEAT_DESC_FUNCS: Dict[str, Tuple[str, Dict[int, Tuple[int, int]], torch.Tensor]]
   'resnet_fc': ('manhattan_distance',RESNET_FC_IDX, RESNET_FC_FEATS),
 }
 
+FEATURES_DIR = 'features'
+os.makedirs(FEATURES_DIR, exist_ok=True)
+FEATURES_FN = FEATURES_DIR + '/{feat_space}.pkl'
+LATENT_FEATURES_FN = FEATURES_DIR + '/task{task}_{feat_space}_{dim_red}_{K}.pkl'
+
 LATENT_SEMANTICS_DIR = 'latent_semantics'
 os.makedirs(LATENT_SEMANTICS_DIR, exist_ok=True)
 LATENT_SEMANTICS_FN = 'latent_semantics/task{task}_{feat_space}_{dim_red}_{K}.pkl'
-LATENT_SEMANTICS_MODES_FN = 'latent_semantics/task{task}_{feat_space}_{dim_red}_{K}_{mode}.pkl'
 
 SIMI_MAT_DIR = 'similarity_matrices'
 os.makedirs(SIMI_MAT_DIR, exist_ok=True)
--- task3.py
+++ task3.py
@@ -20,31 +20,15 @@ def main():
   inp = helper.get_user_input('feat_space,K,dim_red')
   _tmp = config.FEAT_DESC_FUNCS[inp['feat_space']]
   feat_db, idx_dict = _tmp[config.FEAT_DB], _tmp[config.IDX]
-  W, H = dimensionality_reduction.reduce_(feat_db, inp['K'], inp['dim_red'])
+  W, H = dimensionality_reduction.reduce_(feat_db, inp['K'], inp['dim_red'])  # W: img latent feature vectors, H: latent to original feature vectors
 
-    # Using scikit-learn SVD
-    print("Using scikit-learn SVD:")
-    print_img_id_weight_pairs(W, idx_dict)
-    
-    
-    # Using custom SVD
-    W_custom, H_custom = dimensionality_reduction.reduce_(feat_db, inp['K'], 'svd_custom')
-    print("\nUsing custom SVD:")
-    print_img_id_weight_pairs(W_custom, idx_dict)
-    
-    # Using scikit-learn NMF
-    print("Using scikit-learn NMF:")
-    print_img_id_weight_pairs(W, idx_dict)
+  # save the latent to original feature vector matrix
+  helper.save_pickle(H, config.LATENT_SEMANTICS_FN.format(task=3, **inp))
+  # save img latent feature vectors
+  helper.save_pickle(W, config.LATENT_FEATURES_FN.format(task=3, **inp))
 
-    # Using custom NMF 
-    W_custom, H_custom= dimensionality_reduction.reduce_(feat_db, inp['K'], 'nnmf_custom')
-    
-    print("Using custom NMF:")
-    print_img_id_weight_pairs(W_custom,idx_dict)
-    
-    
-    # Save the results from custom nnmf
-    helper.save_pickle(H_custom, config.LATENT_SEMANTICS_FN.format(task=3, feat_space=inp['feat_space'], K=inp['K'], dim_red='nnmf_custom'))
+
+  print_img_id_weight_pairs(W, idx_dict)
 
 
 if __name__ == '__main__':
