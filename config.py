cf533a5cfe52b022c7423078f635a78dd1b34c13
Rohan Awhad <rohanavad007@gmail.com>
2023-10-15 13:22:23 -0700

    removed commented code

--- config.py
+++ config.py
@@ -32,41 +32,17 @@ print('loading resnet models ...')
 MODEL_NAME = 'ResNet50_Weights.DEFAULT'
 RESNET_MODEL = torchvision.models.resnet50(weights=MODEL_NAME).eval()
 
-'''
-print('loading embeddings ... ')
-COLOR_MMT_IDX, COLOR_MMT_FEATS = helper.load_data('color_moment')
-HOG_IDX, HOG_FEATS = helper.load_data('hog')
-RESNET_AVGPOOL_IDX, RESNET_AVGPOOL_FEATS = helper.load_data('resnet_avgpool')
-RESNET_LAYER3_IDX, RESNET_LAYER3_FEATS = helper.load_data('resnet_layer3')
-RESNET_FC_IDX, RESNET_FC_FEATS = helper.load_data('resnet_fc')
 
-# preprocessing feats db
-if not (COLOR_MMT_FEATS is None or isinstance(COLOR_MMT_FEATS, torch.Tensor)): COLOR_MMT_FEATS = torch.tensor(COLOR_MMT_FEATS)
-if not (HOG_FEATS is None or isinstance(HOG_FEATS, torch.Tensor)): HOG_FEATS = torch.tensor(HOG_FEATS)
-if not (RESNET_AVGPOOL_FEATS is None or isinstance(RESNET_AVGPOOL_FEATS, torch.Tensor)): RESNET_AVGPOOL_FEATS = torch.tensor(RESNET_AVGPOOL_FEATS)
-if not (RESNET_LAYER3_FEATS is None or isinstance(RESNET_LAYER3_FEATS, torch.Tensor)): RESNET_LAYER3_FEATS = torch.tensor(RESNET_LAYER3_FEATS)
-if not (RESNET_FC_FEATS is None or isinstance(RESNET_FC_FEATS, torch.Tensor)): RESNET_FC_FEATS = torch.tensor(RESNET_FC_FEATS)
-'''
-
-# ---
+# === Feature Store ===
+from feature_descriptor import FeatureStore
 SIMILARITY_METRIC = 0
 IDX = 1
 FEAT_DB = 2
 
-'''
-FEAT_DESC_FUNCS: Dict[str, Tuple[str, Dict[int, Tuple[int, int]], torch.Tensor]] = {
-  'color_moment': ('pearson_coefficient',COLOR_MMT_IDX, COLOR_MMT_FEATS),
-  'hog': ('intersection_similarity',HOG_IDX, HOG_FEATS),
-  'resnet_avgpool': ('cosine_similarity',RESNET_AVGPOOL_IDX, RESNET_AVGPOOL_FEATS),
-  'resnet_layer3': ('cosine_similarity',RESNET_LAYER3_IDX, RESNET_LAYER3_FEATS),
-  'resnet_fc': ('manhattan_distance',RESNET_FC_IDX, RESNET_FC_FEATS),
-}
-'''
-from feature_descriptor import FeatureStore
 # TODO (rohan): update var name
-#feature_store = FeatureStore(helper.load_data)
 FEAT_DESC_FUNCS = FeatureStore(helper.load_data)
 DISTANCE_MEASURES = ['manhattan_distance', 'kl_divergence']
+# ===
 
 FEATURES_DIR = 'features'
 os.makedirs(FEATURES_DIR, exist_ok=True)
