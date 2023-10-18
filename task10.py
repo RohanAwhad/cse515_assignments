dea1310b67c525f471ce3e8b50429971f3e801d9
Rohan Awhad <rohanavad007@gmail.com>
2023-10-18 16:00:06 -0700

    changed get_similarity_mat_x_mat to get_similarity

--- task10.py
+++ task10.py
@@ -44,14 +44,14 @@ def main():
         inp['dim_red'] = inp_2['dim_red']
         latent_space = helper.load_semantics(f"task{inp['task_id']}_label_label_simi_mat_{inp['feat_space']}_{inp['dim_red']}_{inp['K_latent']}.pkl")
         feat_db_labels = get_label_vecs(inp['feat_space'])
-        query_feat = get_similarity_mat_x_mat(query_feat.unsqueeze(0), feat_db_labels, similarity_metric)
+        query_feat = get_similarity(query_feat, feat_db_labels, similarity_metric)
         feat_db = get_similarity_mat_x_mat(feat_db, feat_db_labels, similarity_metric)
     
     elif inp['task_id'] == 6:
         inp_2 = helper.get_user_input('dim_red', len(config.DATASET))
         inp['dim_red'] = inp_2['dim_red']
         latent_space = helper.load_semantics(f"task{inp['task_id']}_img_img_simi_mat_{inp['feat_space']}_{inp['dim_red']}_{inp['K_latent']}.pkl")
-        query_feat = get_similarity_mat_x_mat(query_feat.unsqueeze(0), feat_db, similarity_metric)
+        query_feat = get_similarity(query_feat, feat_db, similarity_metric)
         feat_db = get_similarity_mat_x_mat(feat_db, feat_db, similarity_metric)
 
     if inp['dim_red'] == 'svd': similarity_metric = 'cosine_similarity'
